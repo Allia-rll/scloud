50c285558665b80a4e6abf42200f940ccffb3cae
lucad-28 <lucadev.rll28@gmail.com>
2024-06-30 00:40:54 -0500

    Old Files deleted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./auth/login/Login";
 import Layout from "./components/Layout";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Dashboard from "./dashboard/Dashboard";
 import NotProtectedRoute from "./components/NotProtectedRoute";
 import InstanceSection from "./InstanceSection/InstanceSection";
 import CiaSection from "./CiasSection/CiaSection";
@@ -30,19 +29,10 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route element={<NotProtectedRoute />}>
-            <Route path="login" element={<Login />} />
-          </Route>
-
-          <Route element={<ProtectedRoute />}>
-            <Route path="dashboard" element={<Dashboard />} />
-          </Route>
-
           <Route path="/" element={<InstanceSection />} />
           <Route path="/credential" element={<CiaSection />} />
           <Route path="/credential/cia" element={<ProjectSection />} />
           <Route path="/credential/cia/proyecto" element={<FilesSections />} />
-
           <Route path="*" element={<Navigate to={"/"} />} />
         </Route>
       </Routes>
